3ff21c626f7c0480787f41ccede76677c48af131
Asim <asimnipty@yahoo.com>
2022-09-02 23:15:25 +0600

    commit state

---
+++ src/components/Top.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+function Top() {
+  return (
+    <div className="top-nav">
+        
+        <div className='top-nav-button'>
+            <button className='button-top'> Fun</button>
+            <button className='button-top'> Games</button>
+            <button className='button-top'> Audio</button>
+            <button className='button-top'> Video</button>
+        </div>
+      
+    </div>
+  );
+}
+
+export default Top;
